11331089e2f9f4a3e41b2a897ded739a1b2b4eee
chenguoqing <15601227311@163.com>
2022-11-25 17:08:30 +0800

    Initial commit

---
+++ src/test/java/org/demo/brower/BrowerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.brower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
